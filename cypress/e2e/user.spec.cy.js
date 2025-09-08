8817eb7bd5c83dd3e65eb5c2b7ddbc5b26bd645e
Pietra Mucciolo <pietra.mucciolo@hotmail.com>
2025-09-07 22:50:55 -0300

    Add page objects

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,43 +1,51 @@
 import userData from '../fixtures/userData.json'
+import LoginPage from '../pages/loginPage'
+import DashboardPage from '../pages/dashboardPage.js'
+
+const loginPage = new LoginPage()
+const dashboardPage = new DashboardPage()
 
 describe('Orange HRM Tests', () => {
 
   const selectorsList = {
-    usernameField: "[name='username']",
-    passwordField: "[name='password']",
-    loginButton: "[type='submit']",
-    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
-    dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: "[role='alert']",
-    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
     firstNameField: "[name='firstName']",
+    middleNameField: "[name='middleName']",
     lastNameField: "[name='lastName']",
     genericField: ".oxd-input--active",
     dateField: "[placeholder='yyyy-mm-dd']",
+    genericComboBox: "oxd-select-text-arrow",
+    secondItemCombobox: ".oxd-select-dropdown > :nth-child(2)",
+    thirdItemCombobox: ".oxd-select-dropdown > :nth-child(3)",
+    fourthItem: ":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-input",
     dateCloseButton: ".--close",
-    submitButton: "[type='submit']"
+    submitButton: ".orangehrm-left-space"
+  
   }
 
   it.only('User Info Update - Success', () => {
+    loginPage.accessLoginPage()
+    loginPage.loginWithAnyUser(userData.userSuccess.username, userData.userSuccess.password)
 
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSucess.username)
-    cy.get(selectorsList.passwordField).type(userData.userSucess.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.dashboardGrid)
-    cy.get(selectorsList.myInfoButton).click()
+    dashboardPage.checkDashboardPage()
+
+    MenuPage.accessMyInfo()
+
     cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
+    cy.get(selectorsList.middleNameField).clear().type('MiddleNameTest')
     cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
-    cy.get(selectorsList.genericField).eq(3).clear().type('Nickname')
-    cy.get(selectorsList.genericField).eq(4).clear().type('Employee')
-    cy.get(selectorsList.genericField).eq(5).clear().type('OtherIdTest')
-    cy.get(selectorsList.genericField).eq(6).clear().type('DriversLicense')
-    cy.get(selectorsList.genericField).eq(7).clear().type('2025-03-10')
+    cy.get(selectorsList.genericField).eq(5).clear().type('Employee')
+    cy.get(selectorsList.genericField).eq(4).clear().type('IdTest')
+    cy.get(selectorsList.fourthItem).clear().type('2025-03-10')
     cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.genericField).eq(8).clear().type('2025-09-02')
-    cy.get(selectorsList.genericField).eq(9).clear().type('2025-09-04')
     cy.get(selectorsList.submitButton).eq(0).click()
+    cy.get('body').should('contain','Sucessfully Updated')
+    cy.get('.oxd-toast-close')
+    
+    cy.get(selectorsList.genericComboBox).eq(0).click()
+    cy.get(selectorsList.secondItemCombobox).click()
+    cy.get(selectorsList.genericComboBox).eq(1).click()
+    cy.get(selectorsList.thirdItemCombobox).click
+    
   })
     it('Login - Fail', () => {
     cy.visit('/auth/login')
